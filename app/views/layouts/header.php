a79823c7a642deab846f5c6eccf4f063395b85f2
sminnaar <sminnaar@student.wethinkcode.co.za>
2019-12-17 14:15:08 +0200

    Added author file

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -4,9 +4,9 @@
     <hr>
     <div class='bar black card'>
       <a href='profile' class='bar-item button'><img style='width: 50px; margin-right: 35px' src='img/profile/def4.jpg'></a>
-      <a href='settings' class='bar-item button'><img style='width: 53px; margin-right: 35px' src='img/icons/settings.png'></a>
       <a href='gallery' class='bar-item button'><img style='width: 50px; margin-right: 35px' src='img/icons/Files.png'></a>
       <a href='upload' class='bar-item button'><img style='width: 55px; margin-right: 35px' src='img/icons/Camera.png'></a>
+      <a href='settings' class='bar-item button'><img style='width: 53px; margin-right: 35px' src='img/icons/settings.png'></a>
       <a href='logout' class='bar-item button'><img style='width: 53px' src='img/icons/Exit.png'></a>
     </div>
     <hr>
